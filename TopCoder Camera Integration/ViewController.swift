cce396f6a656ee90c35261c69e02c9f3bc4bfd70
Doug Harper <doug@zewkey.com>
2015-09-10 17:24:38 -0500

    Working app!
    Completed the storyboard and code to get the app working.

--- TopCoder Camera Integration/ViewController.swift
+++ TopCoder Camera Integration/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cameraButtonTapped(sender: UIButton) {
+        print("button tapped")
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .Camera
+        
+        presentViewController(picker, animated: true, completion: nil)
+        imageView.hidden = false
+    }
 
+    @IBAction func photosButtonTapped(sender: UIButton) {
+        print("button tapped")
+        imageView.hidden = false
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imageView.image = (info[UIImagePickerControllerOriginalImage] as! UIImage)
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
