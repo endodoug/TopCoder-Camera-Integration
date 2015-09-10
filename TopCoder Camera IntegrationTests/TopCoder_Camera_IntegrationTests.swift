81609f48b5ff36426058773207a303b000084c34
Doug Harper <doug@zewkey.com>
2015-09-09 23:30:09 -0500

    Initial Commit

---
+++ TopCoder Camera IntegrationTests/TopCoder_Camera_IntegrationTests.swift
@@ -0,0 +1,36 @@
+//
+//  TopCoder_Camera_IntegrationTests.swift
+//  TopCoder Camera IntegrationTests
+//
+//  Created by doug harper on 9/9/15.
+//  Copyright © 2015 Doug Harper. All rights reserved.
+//
+
+import XCTest
+@testable import TopCoder_Camera_Integration
+
+class TopCoder_Camera_IntegrationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ TopCoder Camera IntegrationUITests/TopCoder_Camera_IntegrationUITests.swift
@@ -0,0 +1,34 @@
+//
+//  TopCoder_Camera_IntegrationUITests.swift
+//  TopCoder Camera IntegrationUITests
+//
+//  Created by doug harper on 9/9/15.
+//  Copyright © 2015 Doug Harper. All rights reserved.
+//
+
+import XCTest
+
+class TopCoder_Camera_IntegrationUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
